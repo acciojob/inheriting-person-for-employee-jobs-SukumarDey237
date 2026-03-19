5d3b1fda462ef4c701f9422297a292b94facb2e6
Sukumar dey <dey.sukumar1999@gmail.com>
2026-03-19 13:34:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 // complete this js code
-class Person{
-	constructor(name,age){
-		this.name = name
-		this.age = age
-	} 
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
+// class Person{
+// 	constructor(name,age){
+// 		this.name = name
+// 		this.age = age
+// 	} 
+// 	greet(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+// 	}
+// }
+
+// class Employee extends Person {
+// 	constructor(name,age,jobTitle) {
+// 		super(name,age)
+// 		this.jobTitle = jobTitle
+// 	}
+// 	jobGreet(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+// 	}
+// }
+function Person(name, age) {
+    return {
+        name,
+        age,
+        greet() {
+            console.log(`Hello, my name is ${name}, I am ${age} years old.`)
+        }
+    }
 }
 
-class Employee extends Person {
-	constructor(name,age,jobTitle) {
-		super(name,age)
-		this.jobTitle = jobTitle
-	}
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-	}
+function Employee(name, age, jobTitle) {
+    const person = Person(name, age)
+    return {
+        ...person,
+        jobTitle,
+        jobGreet() {
+            console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}`)
+        }
+    }
 }
+
 
 // Do not change code below this line
 window.Person = Person;
